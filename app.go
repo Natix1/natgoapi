9b1122250a2fe4dddc07b9bb45c2c6eba478e010
Natix1 <natix1.spam@gmail.com>
2025-01-03 19:15:52 +0100

    Headers endpoint

--- app.go
+++ app.go
@@ -5,6 +5,8 @@ import (
 	"github.com/didip/tollbooth/v8"
 	"github.com/didip/tollbooth/v8/limiter"
 	"github.com/gin-gonic/gin"
+	"fmt"
+	"strings"
 )
 
 var lmt *limiter.Limiter
@@ -26,6 +28,19 @@ func HelloHandler(c *gin.Context) {
 	c.String(http.StatusOK, "Hello, World!\nYour IP address: " + c.GetHeader("CF-Connecting-IP"))
 }
 
+func HeaderHandler(c *gin.Context) {
+	var headersStr strings.Builder
+
+	headers := c.Request.Header
+	for key, values := range headers {
+		for _, value := range values {
+			headersStr.WriteString(fmt.Sprintf("%s: %s\n", key, value))
+		}
+	}
+
+	c.String(http.StatusOK, headersStr.String())
+}
+
 func main() {
 	initRateLimiter()
 
@@ -45,6 +60,6 @@ func main() {
 	r.Use(rateLimitMiddleware)
 
 	r.GET("/", HelloHandler)
-
+	r.GET("/headers", HeaderHandler)
 	r.Run(":5000")
 }
